e72ecd07cd602c546e8cf86bf4c390c22d955a75
BRANIAC <dimmat97@gmail.com>
2016-03-08 05:17:59 +0200

    added query parsing function

--- WebContent/js/details.js
+++ WebContent/js/details.js
@@ -3,4 +3,25 @@ $(document).ready(function() {
 	var ENDPOINT = "http://localhost:3000";
 	var filmsENDPOINT = ENDPOINT + "/films" ;
 
+	var QueryString = function () {
+	 
+	  var query_string = {};
+	  var query = window.location.search.substring(1);
+	  var vars = query.split("?");
+	  for (var i=0;i<vars.length;i++) {
+	    var pair = vars[i].split("=");
+	    if (typeof query_string[pair[0]] === "undefined") {
+	      query_string[pair[0]] = decodeURIComponent(pair[1]);
+	    } else if (typeof query_string[pair[0]] === "string") {
+	      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+	      query_string[pair[0]] = arr;
+	    } else {
+	      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+	    }
+	  } 
+	    return query_string;
+	}();
+
+	console.log(QueryString);
+
 });
